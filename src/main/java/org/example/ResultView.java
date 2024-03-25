9aafc490923713112eb858e2f4058a18770930c7
Kong Heechan <sk980919@naver.com>
2024-03-25 17:32:47 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/org/example/ResultView.java
+++ src/main/java/org/example/ResultView.java
@@ -5,13 +5,6 @@ public class ResultView {
         System.out.println(getWinners(cars) + "가 최종 우승했습니다.");
     }
 
-    public static void playRacingCarGame(int tryCount, Cars cars) {
-        for (int i = 0; i < tryCount; i++) {
-            cars.move();
-            printCarPositions(cars);
-        }
-    }
-
     public static String getWinners(Cars cars) {
         return String.join(", ", cars.getWinners());
     }
