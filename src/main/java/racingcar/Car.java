47f7f43d950f45407d90d6e8f16143d6fabe1d28
chanhalee <chan771788@gmail.com>
2024-03-25 14:53:33 +0900

    feat(Car) Car 전진 요구사항 테스트 작성 및 구현
    자동차가 전진하면 위치가 1 증가한다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import java.util.Objects;
+
+public class Car {
+
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		this(name, 0);
+	}
+
+	public Car(String name, int position) {
+		this.name = name;
+		this.position = position;
+	}
+
+	public void proceed() {
+		this.position += 1;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Car car = (Car)o;
+		return position == car.position && Objects.equals(name, car.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, position);
+	}
+}
