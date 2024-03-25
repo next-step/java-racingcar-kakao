7c826546f25388e07c4c6628b591ebc74600d05e
ddingmin <ddingminyt@gmail.com>
2024-03-25 18:18:21 +0900

    feat: 요구사항에 맞게 개행 추가

--- src/main/java/racing_car/view/Output.java
+++ src/main/java/racing_car/view/Output.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class Output {
 
     public static void printGameProcessTitle() {
+        System.out.println();
         System.out.println("실행 결과");
     }
 
