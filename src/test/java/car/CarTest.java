749265176051cdd3dca674df42e757c99b926c57
HyewonKkang <gpffps369@gmail.com>
2024-03-25 14:41:03 +0900

    test: 전진 테스트 추가

---
+++ src/test/java/car/CarTest.java
@@ -0,0 +1,22 @@
+package car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarTest {
+    @Test
+    @DisplayName("랜덤 값이 4 이상일 경우 전진한다")
+    void forwardTest() {
+        Car car = new Car();
+        car.run(4);
+        assertTrue(car.getPosition() > 0);
+    }
+
+    @Test
+    @DisplayName("랜덤 값이 3 이하일 경우 멈춘다")
+    void stopTest() {
+
+    }
+}
