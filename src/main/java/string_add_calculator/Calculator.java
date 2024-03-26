707b777e829d14076c9d07e5066b2a3a43c2cbf7
ddingmin <ddingminyt@gmail.com>
2024-03-26 16:45:42 +0900

    feat: String 형태의 계산기 기능 추가

--- src/main/java/string_add_calculator/Calculator.java
+++ src/main/java/string_add_calculator/Calculator.java
@@ -3,8 +3,14 @@ package string_add_calculator;
 import java.util.List;
 
 public class Calculator {
+
     public Number sum(List<Number> numbers) {
         return numbers.stream()
                 .reduce(new Number(0), Number::add);
+    }
+
+    public Number sum(String input) {
+        List<Number> numbers = Parser.parseNumbers(input);
+        return sum(numbers);
     }
 }
