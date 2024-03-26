d3c2860573031f1e5fb25df481fb3df2875760cb
ddingmin <ddingminyt@gmail.com>
2024-03-26 11:11:51 +0900

    feat: number 클래스 작성
    - 덧셈 기능
    - 음수/숫자가 아닌 경우 예외 발생

---
+++ src/main/java/string_add_calculator/Number.java
@@ -0,0 +1,39 @@
+package string_add_calculator;
+
+import java.util.Objects;
+
+public class Number {
+    private final int number;
+
+    public Number(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    public Number(String number) {
+        this(Integer.parseInt(number));
+    }
+
+    private void validate(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수가 될 수 없습니다.");
+        }
+    }
+
+    public Number add(Number targetNumber) {
+        return new Number(this.number + targetNumber.number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
