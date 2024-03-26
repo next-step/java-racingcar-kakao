aae3ebfc8a632e9115e7e2226dad5a8c1cba4729
rimrim990 <lim990718@naver.com>
2024-03-26 16:55:58 +0900

    refactor: 공통되는 인스턴스 분리

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,16 +2,23 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("문자열 계산기 단위 테스트")
 public class StringCalculatorTest {
 
+	StringCalculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new StringCalculator();
+	}
+
 	@Test
 	@DisplayName("빈 문자열 또는 null을 입력할 경우 0을 반환한다.")
 	void 빈문자열_혹은_null() {
-		StringCalculator calculator = new StringCalculator();
 		assertThat(calculator.calculate("")).isEqualTo(0);
 		assertThat(calculator.calculate(null)).isEqualTo(0);
 	}
@@ -19,14 +26,12 @@ public class StringCalculatorTest {
 	@Test
 	@DisplayName("숫자 하나를 입력할 경우 해당 숫자를 반환한다.")
 	void 숫자하나() {
-		StringCalculator calculator = new StringCalculator();
 		assertThat(calculator.calculate("1")).isEqualTo(1);
 	}
 
 	@Test
 	@DisplayName("기본 구분자를 기준으로 분리해 합을 계산한다.")
 	void 기본구분자_분리() {
-		StringCalculator calculator = new StringCalculator();
 		assertThat(calculator.calculate("1,2")).isEqualTo(3);
 		assertThat(calculator.calculate("1,2,3")).isEqualTo(6);
 		assertThat(calculator.calculate("1,2:3")).isEqualTo(6);
@@ -35,14 +40,12 @@ public class StringCalculatorTest {
 	@Test
 	@DisplayName("커스텀 구분자를 기준으로 분리해 합을 계산한다.")
 	void 커스텀구분자_분리() {
-		StringCalculator calculator = new StringCalculator();
 		assertThat(calculator.calculate("//;\n1,2;3")).isEqualTo(6);
 	}
 
 	@Test
 	@DisplayName("숫자 이의외 값을 전달할 경우 RuntimeException 예외를 던진다.")
 	void 숫자_이외의_값() {
-		StringCalculator calculator = new StringCalculator();
 		assertThatExceptionOfType(RuntimeException.class)
 			.isThrownBy(() -> calculator.calculate("ㄱ,1,2"));
 	}
@@ -50,7 +53,6 @@ public class StringCalculatorTest {
 	@Test
 	@DisplayName("음수 값을 전달할 경우 RuntimeException 예외를 던진다.")
 	void 음수_값() {
-		StringCalculator calculator = new StringCalculator();
 		assertThatExceptionOfType(RuntimeException.class)
 			.isThrownBy(() -> calculator.calculate("1,-3,2"));
 	}
