de4d406a2eb4bcbdcdb180cc892fde561cbc93bd
HyewonKkang <gpffps369@gmail.com>
2024-03-26 11:07:14 +0900

    feat: 커스텀 구분자 추가 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,24 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
     private static String SEPARATOR = ",|:";
+    private static String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
 
     public int calculate(String input) {
-        return Arrays.stream(input.split(SEPARATOR))
+        Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(input);
+        if (matcher.find()) {
+            SEPARATOR += "|" + matcher.group(1);
+            return add(matcher.group(2).split(SEPARATOR));
+        }
+        return add(input.split(SEPARATOR));
+    }
+
+    private int add(String[] numbers) {
+        return Arrays.stream(numbers)
                 .map(Integer::parseInt).reduce(Integer::sum)
                 .orElse(0);
     }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,16 +1,43 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 합을 계산한다.")
     void defaultSeparatorTest() {
-        Calculator calculator = new Calculator();
         String input = "1,2:3,4:5,6";
+
+        int result = calculator.calculate(input);
+
+        assertEquals(21, result);
+    }
+
+    @Test
+    @DisplayName("사용자 지정 구분자를 사용할 수 있다.")
+    void customSeparatorTest() {
+        String input = "//;\n1;2;3;4;5;6";
+
+        int result = calculator.calculate(input);
+
+        assertEquals(21, result);
+    }
+
+    @Test
+    @DisplayName("사용자 지정 구분자를 추가할 수 있다.")
+    void defaultAndCustomSeparatorTest() {
+        String input = "//;\n1;2:3;4,5;6";
 
         int result = calculator.calculate(input);
 
