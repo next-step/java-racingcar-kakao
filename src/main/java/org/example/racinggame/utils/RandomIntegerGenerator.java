6b097e227bfc236b926643c70e4ef001d2fd5bf7
Kong Heechan <sk980919@naver.com>
2024-03-27 16:36:34 +0900

    refactor: 상수 위치 변경

--- src/main/java/org/example/racinggame/utils/RandomIntegerGenerator.java
+++ src/main/java/org/example/racinggame/utils/RandomIntegerGenerator.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomIntegerGenerator {
 
+    private static final Random random = new Random();
+
     private RandomIntegerGenerator() {
         // utility class
     }
-
-    private static final Random random = new Random();
 
     public static int generateEndInclusive(int start, int end) {
         return random.nextInt(end - start + 1) + start;
