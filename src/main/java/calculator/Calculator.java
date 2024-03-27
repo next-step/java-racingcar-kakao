9f9a45e5938ffdfef77f3b68ba6daee75fad8714
bigpel66 <bigpel66@icloud.com>
2024-03-27 17:07:54 +0900

    refactor: Calculator 실행 로직 및 검증 개선 - Calculator 앱 이름 변경 - main 실행에서 메서드 실행으로 변경함에 따라 인자 검증에 사용되는 메서드 원형을 변경 - 요구사항에 나타난대로 RuntimeException이 발생 시 0을 반환

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.Objects;
+
+public class Calculator {
+    public static int sum(String input) {
+        try {
+            validate(input);
+            return Adder.sum(null);
+        } catch (RuntimeException ex) {
+            return 0;
+        }
+    }
+
+    private static void validate(String input) {
+        checkNonBlank(Objects.requireNonNull(input));
+    }
+
+    private static void checkNonBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력 값이 존재하지 않습니다.");
+        }
+    }
+}
