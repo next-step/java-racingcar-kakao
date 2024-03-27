e91d3aaf7f0cef8ab169909c89548a9e99d1be54
bigpel66 <bigpel66@icloud.com>
2024-03-28 05:19:23 +0900

    feat: Car 객체 생성 - 자동차가 이름을 갖도록 설정 및 이름의 입력 값의 길이 등을 검증 - 자동차가 4이상의 값에서 전진 가능 - 자동차의 상태는 이름과 위치 값으로 표현하도록 구성

---
+++ src/main/java/racinggame/Car.java
@@ -0,0 +1,58 @@
+package racinggame;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Car {
+    private static final Random ENGINE = new Random();
+    private static final int ENGINE_LIMIT = 10;
+    private static final int FORWARDABLE = 4;
+    private static final int MAX_NAME_LENGTH = 5;
+    private final String name;
+    private int offset;
+
+    protected Car(String name) {
+        this.name = name;
+        validateOwner();
+    }
+
+    private void validateOwner() {
+        checkNullOrBlank();
+        checkLength();
+    }
+
+    private void checkNullOrBlank() {
+        Objects.requireNonNull(name);
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("자동차 소유주는 빈 문자열일 수 없습니다.");
+        }
+    }
+
+    private void checkLength() {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 소유주는 5글자 이하여야 합니다.");
+        }
+    }
+
+    public static Car of(String owner) {
+        return new Car(owner);
+    }
+
+    public int offset() {
+        return offset;
+    }
+
+    public void move() {
+        if (pickEngineValue() >= FORWARDABLE) {
+            offset++;
+        }
+    }
+
+    protected int pickEngineValue() {
+        return ENGINE.nextInt(ENGINE_LIMIT);
+    }
+
+    public String status() {
+        return name + " : " + "-".repeat(offset);
+    }
+}
