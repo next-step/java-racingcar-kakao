f007ad7fb63f6fc7d092e1a9152ecfaa715e2310
HyewonKkang <gpffps369@gmail.com>
2024-03-27 14:09:32 +0900

    refactor: 0 이상의 숫자 예외처리 메서드 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -29,12 +29,16 @@ public class Calculator {
     private int validateInput(String input) {
         try {
             int number = Integer.parseInt(input);
-            if (number >= 0) {
-                return number;
-            }
-            throw new RuntimeException("0 이상의 숫자를 입력해야 합니다.");
+            checkPositiveOrThrow(number);
+            return number;
         } catch (NumberFormatException e) {
             throw new RuntimeException("숫자만을 입력해야 합니다.");
+        }
+    }
+
+    private void checkPositiveOrThrow(int number) {
+        if (number < 0) {
+            throw new RuntimeException("0 이상의 숫자를 입력해야 합니다.");
         }
     }
 }
