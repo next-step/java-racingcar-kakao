4dc5c68487e4d7904ca90eb57a18e176ce641ebf
BuchuKim <qkqdlek32@gmail.com>
2024-03-28 14:28:23 +0900

    refactor: view 통일

--- src/main/java/view/RacingGameView.java
+++ src/main/java/view/RacingGameView.java
@@ -1,17 +1,23 @@
 package view;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 import model.Car;
 import model.RacingGame;
 
-import java.util.List;
+public class RacingGameView {
+    private final Scanner sc = new Scanner(System.in);
 
-public class CarOutputView {
-    public void printCarNameMessage() {
+    public String getCarNameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.nextLine();
     }
 
-    public void printTryNumberMessage() {
+    public int getNumberInput() {
         System.out.println("시도할 회수는 몇 회인가요?");
+        return sc.nextInt();
     }
 
     public void printResultMessage() {
@@ -26,9 +32,11 @@ public class CarOutputView {
     }
 
     public void printWinnerMessage(RacingGame racingGame) {
-        StringBuilder sb = new StringBuilder();
-        racingGame.getWinners().forEach(car -> sb.append(car.getName()).append(", "));
-        System.out.println(sb.substring(0, sb.length() - 2) + "가 최종 우승했습니다.");
+        System.out.println(
+            racingGame.getWinners().stream()
+            .map(Car::getName)
+            .collect(Collectors.joining(", "))
+            + "가 최종 우승했습니다.");
     }
 
     public void printErrorMessage(String message) {
