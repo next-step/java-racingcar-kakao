c32abbf53e973fc6670b29059bd012d207625ced
impala <dla4051@naver.com>
2024-03-28 15:58:09 +0900

    refactor(StringCalculatorTest) 테스트코드 중복 제거
    - @ParameterizedTest와 @CsvSource를 사용하여 테스트코드 내의 중복을 제거

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,32 +1,23 @@
 package stringcalculator;
 
-import java.util.List;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class StringCalculatorTest {
 
-	@Test
+	@ParameterizedTest
+	@CsvSource(value = {
+			"1,2,3 / 6",
+			"1:2:3 / 6",
+			"1:2,3 / 6",
+			"1,2:3,10 / 16",
+			"1,,3 / 4",
+			"1, ,3 / 4"
+	}, delimiter = '/')
 	@DisplayName("문자열을 입력받아 연산 결과를 반환")
-	public void calculateTest() {
-		String input = "1,2,3";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(6);
-
-		input = "1:2:3";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(6);
-
-		input = "1:2,3";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(6);
-
-		input = "1,2:3,10";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(16);
-
-		input = "1,,3";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(4);
-
-		input = "1, ,3";
-		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(4);
+	void calculateTest(String input, int expected) {
+		Assertions.assertThat(StringCalculator.calculate(input)).isEqualTo(expected);
 	}
 }
