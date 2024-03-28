b345e6d26d87fe1abf72e8271f2535ff652dfe45
impala <dla4051@naver.com>
2024-03-28 16:31:43 +0900

    refactor(StringParserTest) 테스트 코드 중복 제거

--- src/test/java/stringcalculator/StringParserTest.java
+++ src/test/java/stringcalculator/StringParserTest.java
@@ -1,38 +1,32 @@
 package stringcalculator;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringParserTest {
-	@Test
+	@ParameterizedTest
+	@CsvSource(value = {
+			"10,2,3 | 10,2,3",
+			"1:2:3 | 1,2,3",
+			"1:2,3 | 1,2,3",
+			"1,2:3,10 | 1,2,3,10",
+			"1,,3 | 1,0,3",
+			"1, ,3 | 1,0,3"
+	}, delimiter = '|')
 	@DisplayName("default 구분자 테스트")
-	public void parseDefaultDelimiterTest() {
-		String input = "10,2,3";
+	void parseDefaultDelimiterTest(String input, String expected) {
+		List<Integer> expectedList = parseExpected(expected);
+
 		List<Integer> result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(10, 2, 3);
-
-		input = "1:2:3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3);
-
-		input = "1:2,3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3);
-
-		input = "1,2:3,10";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3, 10);
-
-		input = "1,,3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 0, 3);
-
-		input = "1, ,3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 0, 3);
+		Assertions.assertThat(result).containsExactlyElementsOf(expectedList);
 	}
 
 	@Test
@@ -41,37 +35,25 @@ public class StringParserTest {
 		String input = "//;\n1;2;3";
 		List<Integer> result = StringParser.parse(input);
 		Assertions.assertThat(result).containsExactly(1, 2, 3);
-		input = "//;\n1,2,3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3);
-		input = "//;\n1:2:3";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3);
-		input = "//;\n1;2:3,4";
-		result = StringParser.parse(input);
-		Assertions.assertThat(result).containsExactly(1, 2, 3, 4);
-
 	}
 
-	@Test
-	@DisplayName("Error input 테스트")
-	public void parseErrorInputTest() {
-		Assertions.assertThatThrownBy(() -> StringParser.parse("//n1;2;3"))
-			.isInstanceOf(RuntimeException.class);
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"//n1;2;3",
+			"//,;\n1,2,3",
+			"1 1:2:3",
+			"//\n1;2:3,4",
+			"1,a,3",
+			"1,-2,3"
+	})
+	void parseErrorInputTest(String input) {
+		Assertions.assertThatThrownBy(() -> StringParser.parse(input))
+				.isInstanceOf(RuntimeException.class);
+	}
 
-		Assertions.assertThatThrownBy(() -> StringParser.parse("//,;\n1,2,3"))
-			.isInstanceOf(RuntimeException.class);
-
-		Assertions.assertThatThrownBy(() -> StringParser.parse("1 1:2:3"))
-			.isInstanceOf(RuntimeException.class);
-
-		Assertions.assertThatThrownBy(() -> StringParser.parse("//\n1;2:3,4"))
-			.isInstanceOf(RuntimeException.class);
-
-		Assertions.assertThatThrownBy(() -> StringParser.parse("1,a,3"))
-			.isInstanceOf(RuntimeException.class);
-
-		Assertions.assertThatThrownBy(() -> StringParser.parse("1,-2,3"))
-			.isInstanceOf(RuntimeException.class);
+	private static List<Integer> parseExpected(String expected) {
+		return Arrays.stream(expected.split(","))
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
 	}
 }
