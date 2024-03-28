613fefb8bf17e2c8004347aed4f06431e712e5d1
RyooChan <fbcks97@naver.com>
2024-03-28 15:30:47 +0900

    refactor: 문자열 계산기 리팩터링
    - split, sum 분기
    - 객체 재활용

--- src/main/java/caculator/StringCalculator.java
+++ src/main/java/caculator/StringCalculator.java
@@ -4,25 +4,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static Matcher matcher = null;
 
-    public static int splitAndSum(String text) {
-        if (isBlank(text)) {
-            return 0;
-        }
-        return sum(toInts(split(text)));
+    public static int sumProcess(String[] strList) {
+        return sum(toInts(strList));
     }
 
     private static boolean isBlank(String text) {
         return text == null || text.isEmpty();
     }
 
-    private static String[] split(String text) {
+    public static String[] split(String text) {
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (isBlank(text)) {
+            return new String[]{"0"};
+        }
 
-        if (m.find()) {
-            String customDelimiter = m.group(1) + "|,|;";
-            return m.group(2).split(customDelimiter);
+        matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1) + "|,|;";
+            return matcher.group(2).split(customDelimiter);
         }
 
         return text.split(",|;");
