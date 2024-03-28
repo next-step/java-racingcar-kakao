3afd689c5522a8f31e4703f7f5818627ed0077bb
sage-kim <wjddkel328@gmail.com>
2024-03-28 11:34:06 +0900

    refactor(carName): 차 이름 검증 분리

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -2,23 +2,14 @@ package racing.domain;
 
 public class Car {
 
-    private final int MAX_NAME_LENGTH = 5;
-
-    private final String name;
+    private final CarName name;
     private final CarEngine carEngine;
     private int position;
 
     public Car(String name, CarEngine carEngine) {
-        validateName(name);
-        this.name = name;
+        this.name = new CarName(name);
         this.carEngine = carEngine;
         this.position = 0;
-    }
-
-    private void validateName(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("Car name length should be below 6.");
-        }
     }
 
     public void move() {
@@ -32,6 +23,6 @@ public class Car {
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 }
