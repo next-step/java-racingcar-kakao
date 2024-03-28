076d0e043d9d348dfb452c71b946b1ad498ec4e3
impala <dla4051@naver.com>
2024-03-28 14:14:32 +0900

    refactor(ProceedLogic) PROCEED_THRESHOLD 상수 추출
    자동차의 전진여부를 결정하는 정수를 상수로 추출하여 이름을 부여

--- src/main/java/racingcar/ProceedLogic.java
+++ src/main/java/racingcar/ProceedLogic.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 public class ProceedLogic {
+	private static final int PROCEED_THRESHOLD = 3;
 	private final NumberGenerator numberGenerator;
 	public ProceedLogic(NumberGenerator numberGenerator) {
 		this.numberGenerator = numberGenerator;
 	}
 
 	public boolean askProceed() {
-		return numberGenerator.generate() > 3;
+		return numberGenerator.generate() > PROCEED_THRESHOLD;
 	}
 }
