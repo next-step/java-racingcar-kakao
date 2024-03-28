86845cb2dee5a7ecf611cbb69910cab172711159
eunho9703 <eunho9703@gmail.com>
2024-03-28 16:03:50 +0900

    chore: 불필요한 주석 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,19 +14,7 @@ public class InputView {
         validate(userInput);
         return userInput;
 
-        // TODO : retry logic 구현했으나 indent 이슈로 deprecate
-        /**
-        while (true) {
-            try {
-                System.out.println("차입력 : ");
-                String userInput = sc.nextLine();
-                validate(userInput);
-                return userInput;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-         */
+
     }
 
 
