020271f8535a3fbebb289eb11a64288ceaf835be
impala <dla4051@naver.com>
2024-03-28 15:37:57 +0900

    style(RandomNumberGenerator) 멤버필드와 메소드 사이에 공백라인 추가

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomNumberGenerator implements NumberGenerator {
 
 	private final Random random = new Random();
+
 	@Override
 	public int generate() {
 		return random.nextInt(10);
